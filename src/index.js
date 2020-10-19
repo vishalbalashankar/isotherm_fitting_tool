51a6a6563b6fb5d0db8f9df5f832c065ed306a13
vishal <vishal3@ualberta.ca>
2020-10-19 07:28:30 -0600

    Basic form template from react website

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class NameForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state= {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this); 
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value= {this.state.value} 
+          onChange = {this.handleChange} />
+        </label>
+        <input type ="submit" value="Submit" />
+      </form>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <NameForm />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
+// If you want your app to work offlinade and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
