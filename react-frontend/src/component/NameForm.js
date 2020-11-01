b7bc6d8a5325c086ba2a37bb93c2ebc4fea0baa5
vishal <vishal3@ualberta.ca>
2020-10-31 21:34:38 -0600

    refactor and cleanup code

---
+++ react-frontend/src/component/NameForm.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            adsname: '',
+            density: '',
+            adsb1: 'co2',
+            adsb2: 'n2'
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeValue(event,key) {
+        this.setState({[key]: event.target.value})
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.adsname);
+        event.preventDefault();
+    }
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label> Adsorbent Name : </label>
+                    <input
+                        type='text'
+                        value={this.state.adsname} 
+                        onChange={(event) => this.handleChangeValue(event,'adsname')}
+                    />
+                </div>
+                <div>
+                    <label> Density [kg/m3] :</label>
+                    <input
+                        type='number'
+                        value={this.state.density}
+                        onChange={(event) => this.handleChangeValue(event,'density')}
+                        min='100'
+                        max='3000'
+                        step='100' 
+                    />
+                </div>
+                <div>
+                    <label>Adsorbate 1 :</label>
+                    <select value={this.state.adsb1} onChange={(event) =>  this.handleChangeValue(event,'adsb1')} >
+                        <option value="co2">CO2</option>
+                        <option value="n2">N2</option>
+                        <option value="ch4">CH4</option>
+                        <option value="H2">H2</option>
+                        <option value="ar">Ar</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Adsorbate 2 :</label>
+                    <select value={this.state.adsb2} onChange={(event) => this.handleChangeValue(event,'adsb2')}>
+                        <option value="co2">CO2</option>
+                        <option value="n2">N2</option>
+                        <option value="ch4">CH4</option>
+                        <option value="H2">H2</option>
+                        <option value="ar">Ar</option>
+                    </select>
+                </div>
+                <button type='submit'>Submit</button>
+            </form>
+        )
+    }
+}
+
+export default NameForm
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,91 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-class NameForm extends React.Component {
-  constructor(props){
-    super(props);
-    this.state= {
-      adsname: 'Zeolite-13X',
-      density: 1130,
-      porosity: 0.34,
-      adsb1: 'CO2',
-      adsb2: 'N2',
-    };
-    this.handleChangeValue = this.handleChangeValue.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this); 
-  }
-
-  handleChangeValue(event,key) {
-    this.setState({[key]: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.adsname);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h1>Adsorbent Screening Tool</h1>
-        <ul>
-          <label>
-          Adsorbent Name:
-          <input type="text" value= {this.state.adsname} 
-          onChange = {(event) => this.handleChangeValue(event,'adsname')} />
-          </label>
-        </ul>
-        <ul>
-          <label>
-            Density [kg/m3]:
-            <input type="number" value= {this.state.density} min='100' max='3000' step='100'
-            onChange = {(event) => this.handleChangeValue(event,'density')} />
-          </label> 
-        </ul>
-        <ul>
-          <label>
-            Porosity [-]:
-            <input type="number" value= {this.state.porosity} min='0.1' max='0.9' step='0.01'
-            onChange = {(event) => this.handleChangeValue(event,'porosity')} />
-          </label> 
-        </ul>
-        <ul>
-          <label>
-            Adsorbate - 1:
-            <select value={this.state.adsb1} onChange={(event) => this.handleChangeValue(event,'adsb1')}>
-              <option value="co2">CO2</option>
-              <option value="n2">N2</option>
-              <option value="ch4">CH4</option>
-              <option value="h2">H2</option>
-            </select>
-          </label>
-        </ul>
-        <ul>
-          <label>
-            Adsorbate - 2:
-            <select value={this.state.adsb2} onChange={(event) => this.handleChangeValue(event,'adsb2')}>
-              <option value="co2">CO2</option>
-              <option value="n2">N2</option>
-              <option value="ch4">CH4</option>
-              <option value="h2">H2</option>
-            </select>
-          </label>
-        </ul>
-        <ul>
-          <input type ="submit" value="Submit" />
-        </ul>
-      </form>
-    )
-  }
-}
+import App from './App';
 
 ReactDOM.render(
-  <NameForm />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offlinade and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
