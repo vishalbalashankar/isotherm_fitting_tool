55a221d83a7f066e0ff9a47198d4e42577a909d5
vishal <vishal3@ualberta.ca>
2020-11-01 09:46:38 -0700

    Update nameform using react-bootstrap

--- react-frontend/src/component/NameForm.js
+++ react-frontend/src/component/NameForm.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Button} from 'react-bootstrap'
+import {Form, Button} from 'react-bootstrap'
 
 class NameForm extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            adsname: '',
-            density: '',
+            adsname: 'Zeolite-13X',
+            density: '1130',
             adsb1: 'co2',
             adsb2: 'n2'
         }
@@ -25,48 +25,57 @@ class NameForm extends React.Component {
     }
     render() {
         return(
-            <form onSubmit={this.handleSubmit}>
-                <div>
-                    <label> Adsorbent Name : </label>
-                    <input
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                    <Form.Label>Adsorbent Name: </Form.Label>
+                    <Form.Control
                         type='text'
-                        value={this.state.adsname} 
-                        onChange={(event) => this.handleChangeValue(event,'adsname')}
+                        value={this.state.adsname}
+                        onChange={ (event) => this.handleChangeValue(event,'adsname')} 
                     />
-                </div>
-                <div>
-                    <label> Density [kg/m3] :</label>
-                    <input
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Density [kg/m3]: </Form.Label>
+                    <Form.Control
                         type='number'
                         value={this.state.density}
-                        onChange={(event) => this.handleChangeValue(event,'density')}
+                        onChange={ (event) => this.handleChangeValue(event,'density')} 
                         min='100'
                         max='3000'
-                        step='100' 
+                        step='100'
                     />
-                </div>
-                <div>
-                    <label>Adsorbate 1 :</label>
-                    <select value={this.state.adsb1} onChange={(event) =>  this.handleChangeValue(event,'adsb1')} >
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Adsorbate 1: </Form.Label>
+                    <Form.Control
+                        as='select'
+                        value={this.state.adsb1}
+                        onChange={ (event) => this.handleChangeValue(event,'adsb1')}
+                    >
                         <option value="co2">CO2</option>
                         <option value="n2">N2</option>
                         <option value="ch4">CH4</option>
                         <option value="H2">H2</option>
                         <option value="ar">Ar</option>
-                    </select>
-                </div>
-                <div>
-                    <label>Adsorbate 2 :</label>
-                    <select value={this.state.adsb2} onChange={(event) => this.handleChangeValue(event,'adsb2')}>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Adsorbate 2: </Form.Label>
+                    <Form.Control
+                        as='select'
+                        value={this.state.adsb2}
+                        onChange={ (event) => this.handleChangeValue(event,'adsb2')}
+                    >
                         <option value="co2">CO2</option>
                         <option value="n2">N2</option>
                         <option value="ch4">CH4</option>
                         <option value="H2">H2</option>
                         <option value="ar">Ar</option>
-                    </select>
-                </div>
+                    </Form.Control>
+                </Form.Group>
+
                 <Button type='submit'>Submit</Button>
-            </form>
+            </Form>
         )
     }
 }
