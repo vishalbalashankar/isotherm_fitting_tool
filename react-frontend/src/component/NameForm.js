dd79f254ba47c86b518e8eecf67c4130c1e41bac
vishal <vishal3@ualberta.ca>
2020-11-01 10:27:34 -0700

    Include upload file option

--- react-frontend/src/component/NameForm.js
+++ react-frontend/src/component/NameForm.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Form, Button} from 'react-bootstrap'
-
+import {Form, Row, Col, Button} from 'react-bootstrap'
+import './myStyles.css'
 class NameForm extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
             adsname: 'Zeolite-13X',
-            density: '1130',
+            density: '1100',
             adsb1: 'co2',
             adsb2: 'n2'
         }
@@ -45,6 +45,8 @@ class NameForm extends React.Component {
                         step='100'
                     />
                 </Form.Group>
+                <Row>
+                <Col>
                 <Form.Group>
                     <Form.Label>Adsorbate 1: </Form.Label>
                     <Form.Control
@@ -59,6 +61,19 @@ class NameForm extends React.Component {
                         <option value="ar">Ar</option>
                     </Form.Control>
                 </Form.Group>
+                </Col>
+                <Col>
+                <Form.Group>
+                    <Form.File
+                        className="position-relative"
+                        required
+                        name="file"
+                    />
+                </Form.Group>
+                </Col>
+                </Row>
+                <Row>
+                <Col>
                 <Form.Group>
                     <Form.Label>Adsorbate 2: </Form.Label>
                     <Form.Control
@@ -73,8 +88,20 @@ class NameForm extends React.Component {
                         <option value="ar">Ar</option>
                     </Form.Control>
                 </Form.Group>
-
+                </Col>
+                <Col>
+                <Form.Group>
+                    <Form.File
+                        className="position-relative"
+                        required
+                        name="file"
+                    />
+                </Form.Group>
+                </Col>
+                </Row>
+                <Form.Group>
                 <Button type='submit'>Submit</Button>
+                </Form.Group>
             </Form>
         )
     }
