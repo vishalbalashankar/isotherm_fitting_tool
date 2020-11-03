97c888a172622e9ec38b92a5ce34a67c5c339631
vishal <vishal3@ualberta.ca>
2020-11-02 22:27:16 -0700

    Template for plotly isotherm plots

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NameForm from './component/NameForm'
 import IsoTitle from './component/IsoTitle'
+import PlotIsotherms from './component/PlotIsotherms'
 
 function App() {
   return (
     <div className="container">
       <IsoTitle />
       <NameForm />
+      <PlotIsotherms />
     </div>
   );
 }
