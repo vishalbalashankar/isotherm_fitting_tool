e84b6b8cf312d1c3dbc3112c48c913fdb7f8424e
vishal <vishal3@ualberta.ca>
2020-11-06 16:20:56 -0700

    POST request with axios

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import NameForm from './component/NameForm'
-import IsoTitle from './component/IsoTitle'
-import PlotIsotherms from './component/PlotIsotherms'
-import Postlist from './component/Postlist'
+import PostForm from './component/PostForm'
 
 function App() {
   return (
-    <div className="container">
-      <Postlist />
+    <div>
+      <PostForm />
     </div>
   );
 }
