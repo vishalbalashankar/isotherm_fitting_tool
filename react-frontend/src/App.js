73896cdc2d69fae359af3f6868a1c69731be635e
vishal <vishal3@ualberta.ca>
2020-11-06 15:15:33 -0700

    Example of GET Request

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,20 +3,12 @@ import './App.css';
 import NameForm from './component/NameForm'
 import IsoTitle from './component/IsoTitle'
 import PlotIsotherms from './component/PlotIsotherms'
+import Postlist from './component/Postlist'
 
 function App() {
   return (
     <div className="container">
-      <div class="item1">
-        <IsoTitle />
-      </div>
-      <div class="item2">
-      </div>
-      <div class="item3">
-        <NameForm />
-      </div>
-      <div class="item4"></div>
-      <div class="item5"><PlotIsotherms /></div>
+      <Postlist />
     </div>
   );
 }
---
+++ react-frontend/src/component/Postlist.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import axios from 'axios'
+
+class Postlist extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            posts: []
+        }
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            console.log(response)
+            this.setState({posts: response.data})
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    render() {
+        const { posts } = this.state 
+
+        return (
+            <div>
+                Postlist component
+                {
+                    posts.length ? 
+                    posts.map(post => <div key={post.id}>{post.title}</div>) : null
+                }
+            </div>
+        )
+    }
+}
+
+export default Postlist
