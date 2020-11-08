d95f1a6784bee8256ba897b967f39a23246105f4
vishal <vishal3@ualberta.ca>
2020-11-08 11:12:03 -0700

    Add allowable file extension

--- api/src/isotherm-api.py
+++ api/src/isotherm-api.py
@@ -6,13 +6,16 @@ from werkzeug.utils import secure_filename
 import os
 
 UPLOAD_FOLDER = "/Users/vishal/Documents/isotherm_fitting_tool/utility"
-
+ALLOWED_EXTENSIONS = set(["txt", "pdf"])
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 CORS(app)
 
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/api")
 def index():
@@ -36,7 +39,7 @@ def upload_file():
             if file.filename == "":
                 print("No selected file", flush=True)
                 return redirect(request.url)
-            if file:
+            if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 destination = "/".join([target, filename])
                 print(destination, flush=True)
