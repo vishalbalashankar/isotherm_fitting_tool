e0208dd5ddff8bc57ddc34b7bfb3c2a69387d045
vishal <vishal3@ualberta.ca>
2020-11-10 06:45:47 -0700

    Converting to Flask-restful api

--- api/src/isotherm-api.py
+++ api/src/isotherm-api.py
@@ -1,53 +1,51 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, redirect
-from werkzeug.utils import secure_filename
-from flask_cors import CORS
-
+from flask import Flask, request, redirect, jsonify
+from flask_restful import Resource, Api
 import os 
-
-UPLOAD_FOLDER = "/app/userfile_uploads"
-ALLOWED_EXTENSIONS = set(["csv", "xlsx"])
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-
+api = Api(app)
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/api')
-def index():
-    return "Hello World"
+UPLOAD_FOLDER = "/app/userfile_uploads"
+ALLOWED_EXTENSIONS = set(["csv", "xlsx"])
 
-@app.route("/upload-file", methods=["GET", "POST"])
-def upload_file():
-    target = UPLOAD_FOLDER
-    print(target, flush=True)
-    if not os.path.isdir(target):
-        print("Folder not available, creating a new directory", flush=True)
-        os.mkdir(target)
-    if request.method == "POST":
-        # Check if the post request has the file part
-        if "files[]" not in request.files:
-            print("No files[] part exist", flush=True)
-            return redirect(request.url)
-        files = request.files.getlist("files[]")
-        for file in files:
-            if file.filename == "":
-                print("No selected file", flush=True)
+class Upload_File(Resource):
+    def __init__(self):
+        self.output_name = "Upload_File" 
+        super()
+
+    def post(self):
+        target = UPLOAD_FOLDER
+        print(target, flush=True)
+        if not os.path.isdir(target):
+            print("Folder not available, creating a new directory", flush=True)
+            os.mkdir(target)
+        if request.method == "POST":
+            # Check if the post request has the file part
+            if "files[]" not in request.files:
+                print("No files[] part exist", flush=True)
                 return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                destination = "/".join([target, filename])
-                print(destination, flush=True)
-                file.save(destination)
-                print("Uploading incoming file:", filename, flush=True)
-                # return redirect(url_for('uploaded_file',filename=filename))
+            files = request.files.getlist("files[]")
+            for file in files:
+                if file.filename == "":
+                    print("No selected file", flush=True)
+                    return redirect(request.url)
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    destination = "/".join([target, filename])
+                    print(destination, flush=True)
+                    file.save(destination)
+                    print("Uploading incoming file:", filename, flush=True)
+                    # return redirect(url_for('uploaded_file',filename=filename))
+            return ""
 
-    return ""
+api.add_resource(Upload_File,"/upload-file")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=7501, debug=True)
-
-CORS(app, expose_headers="Authorization")
