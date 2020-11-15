3aa36a65c68fc838a69f67e36ae46a2790c74470
vishal <vishal3@ualberta.ca>
2020-11-15 08:32:52 -0700

    Added sub heading

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import NameForm from './component/NameForm'
 import IsoTitle from './component/IsoTitle'
+import FittingTitle from './component/FittingTitle'
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
         <IsoTitle />
       </div>
       <div class="item2">
+        <FittingTitle />
+      </div>
+      <div class="item3">
       </div>
       <div>
         <NameForm />
---
+++ react-frontend/src/component/FittingTitle.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './myStyles.css'; 
+
+class FittingTitle extends React.Component {
+    
+    render() {
+        return(
+          <div className="SubTitle">
+            Isotherm Curve Fitting
+          </div>
+        )
+    }
+}
+
+export default FittingTitle
