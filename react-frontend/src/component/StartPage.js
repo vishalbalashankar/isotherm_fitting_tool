a309410e043c0b56adab5f705050e8040f017572
vishalbalashankar <vishal3@ualberta.ca>
2020-12-30 19:26:25 +0000

    Added text animations in the start page

--- react-frontend/src/component/StartPage.js
+++ react-frontend/src/component/StartPage.js
@@ -1,20 +1,49 @@
-import React from 'react';
-import Button from "react-bootstrap/Button";
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './myStyles.css'
+import Typist from "react-typist";
 
 function StartPage() {
+
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    // document.title = `You clicked ${count} times`;
+    setCount(1);
+  }, [count]);
+
   return (
-    <div>
-        <div class="buttonContainer" >
-            <Link to="/compute_isothermfit">
-                <Button className="btn-size" variant="outline-primary" size="lg">Fit Isotherms</Button>{" "}
-            </Link>
-            <Link to="/enter_isothermfit">
-                <Button className="btn-size" variant="outline-primary" size="lg">Enter Isotherm Values</Button> 
-            </Link>
+    <Container fluid="xl" className="buttonContainer" >
+      <Row >
+        <div className="start-text">
+          {count ? (
+            <Typist avgTypingDelay={70} onTypingDone={() => setCount(0)}>
+              <span> <b>Welcome!!</b> Choose one of the options below to get started</span>
+              <Typist.Backspace count={40} delay={800} />
+              <span> option - 1 to 'fit isotherms' first and start the screening process..</span>
+              <Typist.Backspace count={60} delay={1600} />
+              <span> 2 enter 'isotherm values' directly and start the screening process..</span>
+            </Typist>
+          ) : (
+              ""
+            )}
         </div>
-    </div>
+      </Row>
+      <Row>
+        <Col>
+          <Link to="/compute_isothermfit">
+            <Button className="btn-size" variant="outline-primary mr-1" size="lg">Fit Isotherms</Button>
+          </Link>
+        </Col>
+        <Col>
+          <Link to="/enter_isothermfit">
+            <Button className="btn-size" variant="outline-primary mr-1" size="lg">Enter Isotherm Values</Button>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
